6fcd4ebc0853b87ceff6ef50bb0436c5f5f78a5d
liyufeng <liyufeng@syswin.com>
2019-03-08 10:25:47 +0800

    add v7

--- app/src/main/java/com/lyf/content/web/jpmediawrapper/MainActivity.java
+++ app/src/main/java/com/lyf/content/web/jpmediawrapper/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lyf.content.web.jpmediawrapper;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.cry.mediametaretriverwrapper.MediaMetadataRetrieverWrapper;
+import com.cry.mediametaretriverwrapper.RetrieverProcessThread;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaMetadataRetrieverWrapper metadataRetriever2 = new MediaMetadataRetrieverWrapper();
+        metadataRetriever2.setDataSource("");
+        metadataRetriever2
+                .getFramesInterval(500, 2, new RetrieverProcessThread.BitmapCallBack() {
+                    @Override
+                    public void onComplete(final Bitmap frame) {
+                    }
+
+                    @Override
+                    public void onEnd() {
+                    }
+                });
     }
 }
